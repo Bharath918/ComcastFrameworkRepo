3195520fda7e7545ca9ac6f8fbb7c29fea08b4db
Muthahir26 <muthahir26.mm@gmail.com>
2026-09-21 16:25:09 +0530

    gfg

---
+++ Ninza_HRM/src/test/java/ddt/JsonFileTest.java
@@ -0,0 +1,10 @@
+package ddt;
+
+public class JsonFileTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hiiiiiiiiiiiiiiiiiii");
+	}
+
+}
