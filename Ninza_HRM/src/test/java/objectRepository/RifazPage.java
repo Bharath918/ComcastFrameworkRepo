f839b861e4eab8ea51d30a3ba3c73b2eae5bf2d6
gdsstsystrstrsx <reddyln123@gmail.com>
2026-09-21 18:07:22 +0530

    okay

--- Ninza_HRM/src/test/java/objectRepository/RifazPage.java
+++ Ninza_HRM/src/test/java/objectRepository/RifazPage.java
@@ -21,10 +21,6 @@ public class RifazPage {
 	@FindBy(xpath = "//button[contains(text(),'Sign in')]")
 	private WebElement signinbtn;
 
-	public WebElement getSigninbtn() {
-		return signinbtn;
-		
-	}
 	
 	
 	
